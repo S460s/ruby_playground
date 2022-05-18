875527235b56cdb565f8978b10c94db7ff97ba4f
S460 <rommel.e@protonmail.ch>
2022-05-18 13:54:27 +0300

    onelined

--- sub_string.rb
+++ sub_string.rb
@@ -1,20 +1,15 @@
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 string = "Howdy partner, sit down! How's it going?"
 
+answer = { 'down' => 1, 'go' => 1, 'going' => 1, 'how' => 2, 'howdy' => 1, 'it' => 2, 'i' => 3, 'own' => 1,
+           'part' => 1, 'partner' => 1, 'sit' => 1 }
+
 def substrings(string, dictionary)
   str_hash = Hash.new(0)
-
-  string.split.each do |str|
-    dictionary.each do |word|
-      for i in 1..str.length
-        for j in 0..(str.length - i)
-          str_hash[str[j, i].downcase] += 1 if str[j, i].downcase == word
-        end
-      end
-    end
-  end
+  string.split.each  {|str| dictionary.each {|word| str.length.times {|i| (str.length - i).times {|j| str_hash[str[j, i + 1].downcase] += 1 if str[j, i + 1].downcase == word}}}}
   str_hash
 end
 
 hash = substrings(string, dictionary)
+p(hash == answer)
 p hash
